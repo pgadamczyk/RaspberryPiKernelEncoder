e22dd6d4b7c3a788fa3abcc0c7f6452a586d9d5a
jtabor <jtabor@thalchemy.com>
2021-09-13 13:39:41 -0500

    added reset every 10 seconds to python script

--- readEncodersLoop.py
+++ readEncodersLoop.py
@@ -3,8 +3,14 @@
 import encoders
 import time
 
+RESET_TIME_SEC = 10;
+startTime = time.now()
 while True:
     [l,r] = encoders.readEncoders();
     print("%d,%d" % (l,r));
-    time.sleep(.01);    
+    if (time.now() > startTime + RESET_TIME_SEC):
+        encoders.resetEncoders();
+        startTime = time.now();
+    time.sleep(.01);  
+
 
