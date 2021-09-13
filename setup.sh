9608e766859f0cfc07297f530b6abfdb7f6193ec
jtabor <jtabor@thalchemy.com>
2021-09-13 13:26:43 -0500

    added git clone to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+git clone https://github.com/jtabor/RaspberryPiKernelEncoder.git
 apt -y install raspberrypi-kernel-headers
 apt -y install --reinstall raspberrypi-bootloader raspberypi-kernel raspberrypi-kernel-headers
+cd RaspberryPiKernelEncoder
 sudo -u pi make
 sudo make load
