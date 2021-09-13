8975eebfdff4ea2bb40c77a3aa6b84df7fd73699
jtabor <jtabor@thalchemy.com>
2021-09-13 14:19:23 -0500

    made git repo user owned insted of sudo owned

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 git clone https://github.com/jtabor/RaspberryPiKernelEncoder.git
+sudo chown -R pi RaspberryPiKernelEncoder
 apt -y install raspberrypi-kernel-headers
 apt -y install --reinstall raspberrypi-bootloader raspberypi-kernel raspberrypi-kernel-headers
 cd RaspberryPiKernelEncoder
