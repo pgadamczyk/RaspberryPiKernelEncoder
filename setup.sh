2e169888228fa79a7f887fb6d0a8ffe2216a5bb5
jtabor <jtabor@thalchemy.com>
2021-09-27 14:37:39 -0500

    fixed typo in setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/jtabor/RaspberryPiKernelEncoder.git
 sudo chown -R pi RaspberryPiKernelEncoder
 apt -y install raspberrypi-kernel-headers
-apt -y install --reinstall raspberrypi-bootloader raspberypi-kernel raspberrypi-kernel-headers
+apt -y install --reinstall raspberrypi-bootloader raspberrypi-kernel raspberrypi-kernel-headers
 cd RaspberryPiKernelEncoder
 sudo -u pi make
 sudo make load
